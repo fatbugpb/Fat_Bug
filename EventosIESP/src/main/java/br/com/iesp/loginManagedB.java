210a371735b7ca764358825f778832491bfc845a
Digulas <49574447+RodrigoRamosRezende@users.noreply.github.com>
2019-11-04 16:12:53 -0200

    Gerenciamento de Eventos no IESP

---
+++ EventosIESP/src/main/java/br/com/iesp/loginManagedB.java
@@ -0,0 +1,42 @@
+package br.com.iesp;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.iesp.Usuario;
+import br.com.iesp.UsuarioDados;
+
+@ManagedBean(name = "LoginMB")
+@ViewScoped
+public class loginManagedB {
+	
+	private UsuarioDados usuarioDAO = new UsuarioDados();
+	  private Usuario usuario = new Usuario();
+	   
+	  public String envia() {
+	         
+	    usuario = usuarioDAO.getUsuario(usuario.getNomeUsuario(), usuario.getSenha());
+	    if (usuario == null) {
+	      usuario = new Usuario();
+	      FacesContext.getCurrentInstance().addMessage(
+	         null,
+	         new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!",
+	           "Erro no Login!"));
+	      return null;
+	    } else {
+	          return "/main";
+	    }
+	         
+	         
+	  }
+	 
+	  public Usuario getUsuario() {
+	    return usuario;
+	  }
+	 
+	  public void setUsuario(Usuario usuario) {
+	    this.usuario = usuario;
+	  }
+}
